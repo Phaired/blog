fcd61a976f6358fba82ffd3423a66cdac03e69bd
Phaired <65019388+Phaired@users.noreply.github.com>
2024-10-28 23:34:45 +0100

    feat: compress content (#115)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,32 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://remybarranco.fr',
-	integrations: [mdx(), sitemap()],
-	markdown: {
-		remarkPlugins: [
-			'remark-math',
-		],
-		rehypePlugins: [
-			['rehype-katex', {
-				trust: true
-				// Katex plugin options
-			}]
-		]
-	}
+    site: 'https://remybarranco.fr',
+    integrations: [mdx(), sitemap(), (await import("astro-compress")).default({
+        CSS: true,
+        HTML: {
+            "html-minifier-terser": {
+                removeAttributeQuotes: false,
+            },
+        },
+        Image: false,
+        JavaScript: true,
+        SVG: false,
+    }),
+    ],
+
+    markdown: {
+        remarkPlugins: [
+            'remark-math',
+        ],
+        rehypePlugins: [
+            ['rehype-katex', {
+                trust: true
+                // Katex plugin options
+            }]
+        ]
+    }
 });
